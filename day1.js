95c3fe20873872e7a43e88d006e483e9c8b24246
Louis-André Labadie <monsieur@louis-andre.net>
2018-12-01 09:56:38 -0500

    Add day 1

--- day1.js
+++ day1.js
@@ -1,3 +1,24 @@
-const input = require('fs').readFile('inputs/day1.js')
+const input = require('fs').readFileSync("./inputs/day1.txt").toString()
+const steps = input.split(/\n/)
 
-console.log(input)
+let frequency = 0
+let log = [0]
+let final = undefined
+let matched = undefined
+
+while (matched == null) {
+	steps.forEach(step => {
+		frequency = eval(frequency + step)
+		if (!matched) {
+			if (log.indexOf(frequency) == -1) {
+				log.push(frequency)
+			} else {
+				matched = frequency
+			}
+		}
+	});
+	if (final == undefined) {final = frequency}
+}
+
+console.log("Final frequency: " + final)
+console.log("First match: " + matched)
