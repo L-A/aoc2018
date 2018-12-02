2446283d215ed82558a47bb0ce6a4306145775bc
Louis-André Labadie <monsieur@louis-andre.net>
2018-12-02 09:49:25 -0500

    Tabs → Spaces ¯\_(ツ)_/¯

--- day1.js
+++ day1.js
@@ -7,17 +7,17 @@ let final = undefined
 let matched = undefined
 
 while (matched == null) {
-	steps.forEach(step => {
-		frequency = eval(frequency + step)
-		if (!matched) {
-			if (log.indexOf(frequency) == -1) {
-				log.push(frequency)
-			} else {
-				matched = frequency
-			}
-		}
-	});
-	if (final == undefined) {final = frequency}
+  steps.forEach(step => {
+    frequency = eval(frequency + step)
+    if (!matched) {
+      if (log.indexOf(frequency) == -1) {
+        log.push(frequency)
+      } else {
+        matched = frequency
+      }
+    }
+  });
+  if (final == undefined) {final = frequency}
 }
 
 console.log("Final frequency: " + final)
